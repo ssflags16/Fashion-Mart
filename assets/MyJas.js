8cafa28fede0acfe9403c25d40964026dfadde11
Shahid Siraj <ssflags16@gmail.com>
2023-06-27 19:31:32 +0500

    style and index updated and js file added

---
+++ assets/MyJas.js
@@ -0,0 +1,23 @@
+// var open = document.getElementById('bars');
+// open.addEventListener('click', function() {
+//     var nav = document.getElementsByClassName('links');
+//     nav[0].classList.add('open');
+// })
+// var close = document.getElementsByClassName('close');
+// close[0].addEventListener('click', function() {
+//     var navclose = document.getElementsByClassName('links');
+//     navclose[0].classList.remove('open');
+
+// })
+
+var open = document.getElementById('bars');
+open.addEventListener('click', function() {
+    var nav = document.getElementsByClassName('links');
+    nav[0].classList.add('open');
+})
+var close = document.getElementsByClassName('close');
+close[0].addEventListener('click', function() {
+    var navclose = document.getElementsByClassName('links');
+    navclose[0].classList.remove('open');
+
+})
